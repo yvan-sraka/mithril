fcdc8f156af76c5dbe797187e6a57d204d5f9ae5
abailly-iohk <abailly-iohk@users.noreply.github.com>
2021-10-22 11:44:28 +0000

    deploy: 0592c65d8f43a2c8d876bfa468b461f5d422020e

--- mithril/key_reg/sidebar-items.js
+++ mithril/key_reg/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["RegisterError",""]],"struct":[["KeyReg",""],["RegParty","A registered party, i.e. an id associated with its stake and public key"]]});
+initSidebarItems({"enum":[["RegisterError",""]],"struct":[["KeyReg",""],["RegParty",""]]});
