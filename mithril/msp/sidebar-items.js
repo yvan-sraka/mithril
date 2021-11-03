d62ac677241b73d592c5329d47fd234047bf7653
spaceships <spaceships@users.noreply.github.com>
2021-11-03 20:40:24 +0000

    deploy: 9b0b4089996e1cb380c60f20d4118f9ddb2ca7df

--- mithril/msp/sidebar-items.js
+++ mithril/msp/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Msp","Struct used to namespace the functions."],["MspMvk","MSP verification key."],["MspPk","MSP public key."],["MspSig","MSP signature."],["MspSk","MSP secret key."]]});
+initSidebarItems({"struct":[["Msp","Struct used to namespace the functions. See section 2.4 of the paper."],["MspMvk","MSP verification key."],["MspPk","MSP public key, contains the verification key and proof of posession."],["MspSig","MSP signature."],["MspSk","MSP secret key."]]});
