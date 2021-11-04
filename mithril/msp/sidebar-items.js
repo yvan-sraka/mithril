85f4b0ad8e992adeb9f9a5741fccf8d3504ed440
spaceships <spaceships@users.noreply.github.com>
2021-11-04 18:31:04 +0000

    deploy: b5339ed7e709cbaa48c51666763bebb6e600310c

--- mithril/msp/sidebar-items.js
+++ mithril/msp/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Msp",""],["MspMvk",""],["MspPk",""],["MspSig",""],["MspSk",""]]});
+initSidebarItems({"struct":[["Msp","Struct used to namespace the functions."],["MspMvk","MSP verification key."],["MspPk","MSP public key, contains the verification key and proof of posession."],["MspSig","MSP signature."],["MspSk","MSP secret key."]]});
--- mithril/proof/trivial/sidebar-items.js
+++ mithril/proof/trivial/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["TrivialEnv",""],["TrivialError",""],["TrivialProof",""]]});
+initSidebarItems({"struct":[["TrivialEnv","Trivial environment which contains nothing."],["TrivialError","Error wrapper for TrivialProof."],["TrivialProof","TrivialProof simply contains the Witness directly."]]});
