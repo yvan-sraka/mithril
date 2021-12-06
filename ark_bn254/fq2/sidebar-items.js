9174892623112b92a1a51e883406c5019d7dac28
iquerejeta <iquerejeta@users.noreply.github.com>
2021-12-06 12:00:47 +0000

    deploy: f52d8a28c0c7825d3fc56dae44a078f3facdf7d6

--- ark_bn254/fq2/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"constant":[["FQ2_ONE",""],["FQ2_ZERO",""]],"struct":[["Fq2Parameters",""]],"type":[["Fq2",""]]});
--- ark_bn254/g1/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"constant":[["G1_GENERATOR_X","G1_GENERATOR_X = 1"],["G1_GENERATOR_Y","G1_GENERATOR_Y = 2"]],"struct":[["Parameters",""]]});
