f995d10493403f1f1482aa68e89bac5302797caf
abailly-iohk <abailly-iohk@users.noreply.github.com>
2022-01-19 13:28:31 +0000

    deploy: 89575f79bb8bbb08ced0e12425b66fb39218ab82

--- implementors/mithril/atms/trait.Atms.js
+++ implementors/mithril/atms/trait.Atms.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["bytes"] = [];
+implementors["mithril"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
