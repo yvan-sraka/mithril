f59dde936080cc34dbb49fd4393799a15512a460
jpraynaud <jpraynaud@users.noreply.github.com>
2022-05-11 16:58:17 +0000

    deploy: 9cfa79cce098730d8feb77d560a7c67fc7df1689

--- mithril-core/doc/implementors/core/hash/trait.Hash.js
+++ mithril-core/doc/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["mithril"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"mithril/msp/struct.MspPk.html\" title=\"struct mithril::msp::MspPk\">MspPk</a>","synthetic":false,"types":["mithril::msp::MspPk"]}];
+implementors["mithril"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"mithril/multi_sig/struct.VerificationKey.html\" title=\"struct mithril::multi_sig::VerificationKey\">VerificationKey</a>","synthetic":false,"types":["mithril::multi_sig::VerificationKey"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
