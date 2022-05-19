e5d95f24b543fbc8c3c665f224397861ca74bd9f
jpraynaud <jpraynaud@users.noreply.github.com>
2022-05-19 08:25:21 +0000

    deploy: d896b557b69b3db120dcbece784611671141a635

--- mithril-core/doc/source-files.js
+++ mithril-core/doc/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["mithril"] = {"name":"","files":["c_api.rs","error.rs","key_reg.rs","lib.rs","merkle_tree.rs","multi_sig.rs","stm.rs"]};
+sourcesIndex["mithril"] = {"name":"","files":["dense_mapping.rs","error.rs","key_reg.rs","lib.rs","merkle_tree.rs","multi_sig.rs","stm.rs"]};
 createSourceSidebar();
