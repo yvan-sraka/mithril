3f7a6e5476b8d40b1b06c99ea9aea06f99928610
Alenar <Alenar@users.noreply.github.com>
2022-05-25 13:42:19 +0000

    deploy: e89940357acf14a20c3cff7fef81e6ae847f57a3

--- mithril-common/doc/source-files.js
+++ mithril-common/doc/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["mithril_common"] = {"name":"","dirs":[{"name":"crypto_helper","files":["codec.rs","mod.rs","tests_setup.rs","types.rs"]}],"files":["apispec.rs","entities.rs","fake_data.rs","immutable_digester.rs","lib.rs"]};
+sourcesIndex["mithril_common"] = {"name":"","dirs":[{"name":"crypto_helper","files":["codec.rs","conversions.rs","mod.rs","tests_setup.rs","types.rs"]}],"files":["apispec.rs","entities.rs","fake_data.rs","immutable_digester.rs","lib.rs"]};
 createSourceSidebar();
