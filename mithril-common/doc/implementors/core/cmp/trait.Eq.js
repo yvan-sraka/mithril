c55c1cb1f64760fd59c5bb93712774869349fafb
jpraynaud <jpraynaud@users.noreply.github.com>
2022-05-25 15:36:56 +0000

    deploy: 1cf11135e68df1d067fdbfcf88675ce66778a32d

---
+++ mithril-common/doc/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["mithril_common"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mithril_common/entities/struct.Beacon.html\" title=\"struct mithril_common::entities::Beacon\">Beacon</a>","synthetic":false,"types":["mithril_common::entities::Beacon"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ mithril-common/doc/implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["mithril_common"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"mithril_common/entities/struct.Beacon.html\" title=\"struct mithril_common::entities::Beacon\">Beacon</a>","synthetic":false,"types":["mithril_common::entities::Beacon"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
