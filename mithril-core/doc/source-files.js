8b57309facb18748473618a2a459b7bf4781e000
Alenar <Alenar@users.noreply.github.com>
2022-06-15 09:11:05 +0000

    deploy: b9976b4d280ff35edfd8745d3ad2d082d726d3ee

--- mithril-core/doc/source-files.js
+++ mithril-core/doc/source-files.js
@@ -1,4 +1,3 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["mithril"] = {"name":"","files":["dense_mapping.rs","error.rs","key_reg.rs","lib.rs","merkle_tree.rs","multi_sig_zcash.rs","stm.rs"]};
-sourcesIndex["mithril_client"] = {"name":"","files":["aggregator.rs","entities.rs","lib.rs","runtime.rs","verifier.rs"]};
 createSourceSidebar();
