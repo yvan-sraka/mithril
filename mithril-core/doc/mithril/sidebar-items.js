fb55649da04f64666c3f28a01d944bb46b402bbd
iquerejeta <iquerejeta@users.noreply.github.com>
2022-08-03 08:53:38 +0000

    deploy: a8db7cf27d4a82c516380ef98d5764e668c34fb1

--- mithril-core/doc/mithril/sidebar-items.js
+++ mithril-core/doc/mithril/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["error","Crate specific errors"],["key_reg","Key registration functionality."],["merkle_tree","Creation and verification of Merkle Trees"],["stm","Top-level API for Mithril Stake-based Threshold Multisignature scheme. See figure 6 of the paper for most of the protocol."]]});
+initSidebarItems({"enum":[["AggregationFailure","Error types for aggregation."]],"mod":[["key_reg","Key registration functionality."],["stm","Top-level API for Mithril Stake-based Threshold Multisignature scheme. See figure 6 of the paper for most of the protocol."]]});
