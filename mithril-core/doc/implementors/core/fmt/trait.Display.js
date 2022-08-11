11f3e6945cf917cd4f3973735df05e1621646e20
curiecrypt <curiecrypt@users.noreply.github.com>
2022-08-11 12:09:50 +0000

    deploy: c3a8a3116b71f039c87a894c01e87d4894006c19

--- mithril-core/doc/implementors/core/fmt/trait.Display.js
+++ mithril-core/doc/implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["mithril"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mithril/enum.AggregationFailure.html\" title=\"enum mithril::AggregationFailure\">AggregationFailure</a>","synthetic":false,"types":["mithril::error::AggregationFailure"]}];
+implementors["mithril"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mithril/enum.AggregationError.html\" title=\"enum mithril::AggregationError\">AggregationError</a>","synthetic":false,"types":["mithril::error::AggregationError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- mithril-core/doc/implementors/std/error/trait.Error.js
+++ mithril-core/doc/implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["mithril"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.1/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"mithril/enum.AggregationFailure.html\" title=\"enum mithril::AggregationFailure\">AggregationFailure</a>","synthetic":false,"types":["mithril::error::AggregationFailure"]}];
+implementors["mithril"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.62.1/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"mithril/enum.AggregationError.html\" title=\"enum mithril::AggregationError\">AggregationError</a>","synthetic":false,"types":["mithril::error::AggregationError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
