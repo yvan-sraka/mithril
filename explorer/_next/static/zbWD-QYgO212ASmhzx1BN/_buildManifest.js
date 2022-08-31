249b9cd17c73bf6094727a64d3df4087ddf8f849
Alenar <Alenar@users.noreply.github.com>
2022-08-31 13:17:17 +0000

    deploy: e0916d3de858b96b19f253cc4467b90594fc81ab

--- explorer/_next/static/zbWD-QYgO212ASmhzx1BN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/332-39765bce4b1202a2.js","static/css/c091b0ecb0a16138.css","static/chunks/pages/index-36cae201e84f0793.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
