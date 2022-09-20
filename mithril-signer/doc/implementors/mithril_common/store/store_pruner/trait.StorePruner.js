bdbf456daa0a1880eb29e7be1d4397cb8deb2299
ghubertpalo <ghubertpalo@users.noreply.github.com>
2022-09-20 09:11:52 +0000

    deploy: d4ab3d4031d6b6b2ee306e992dec185b76bf9efc

---
+++ mithril-signer/doc/implementors/mithril_common/store/store_pruner/trait.StorePruner.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["mithril_signer"] = [{"text":"impl StorePruner for <a class=\"struct\" href=\"mithril_signer/struct.ProtocolInitializerStore.html\" title=\"struct mithril_signer::ProtocolInitializerStore\">ProtocolInitializerStore</a>","synthetic":false,"types":["mithril_signer::protocol_initializer_store::ProtocolInitializerStore"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
