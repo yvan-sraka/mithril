effd5814db980f502e61ac69d2b79760e3a5afb4
jpraynaud <jpraynaud@users.noreply.github.com>
2022-11-03 15:47:03 +0000

    deploy: cbb5d839f951132d50b8cac77659aed303cca3b0

--- rust-doc/implementors/config/source/trait.Source.js
+++ rust-doc/implementors/config/source/trait.Source.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["mithril_aggregator"] = [{"text":"impl Source for <a class=\"struct\" href=\"mithril_aggregator/struct.MainOpts.html\" title=\"struct mithril_aggregator::MainOpts\">MainOpts</a>","synthetic":false,"types":["mithril_aggregator::command_args::MainOpts"]},{"text":"impl Source for <a class=\"struct\" href=\"mithril_aggregator/struct.DefaultConfiguration.html\" title=\"struct mithril_aggregator::DefaultConfiguration\">DefaultConfiguration</a>","synthetic":false,"types":["mithril_aggregator::configuration::DefaultConfiguration"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"mithril_aggregator":[["impl Source for <a class=\"struct\" href=\"mithril_aggregator/struct.MainOpts.html\" title=\"struct mithril_aggregator::MainOpts\">MainOpts</a>"],["impl Source for <a class=\"struct\" href=\"mithril_aggregator/struct.DefaultConfiguration.html\" title=\"struct mithril_aggregator::DefaultConfiguration\">DefaultConfiguration</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
