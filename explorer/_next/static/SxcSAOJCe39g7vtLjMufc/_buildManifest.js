4bdb82dfcaabc4fb29074b055dda119d7b939d92
jpraynaud <jpraynaud@users.noreply.github.com>
2022-12-14 16:58:21 +0000

    deploy: d66c0caefadb7ff741fc914bc508c6eb7abbb5d4

---
+++ explorer/_next/static/SxcSAOJCe39g7vtLjMufc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/444-06c17e23ffa4247c.js","static/css/c091b0ecb0a16138.css","static/chunks/pages/index-a4688a3a91c7da07.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
