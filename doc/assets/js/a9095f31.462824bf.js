0d159776a8208fb5f706d993528845d67dbd3f6b
ghubertpalo <ghubertpalo@users.noreply.github.com>
2023-01-12 08:45:22 +0000

    deploy: df6ff6ea00c21e5b8537b601dc58fd9fe6f06113

--- doc/assets/js/a9095f31.462824bf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmithril_doc=self.webpackChunkmithril_doc||[]).push([[745],{9053:e=>{e.exports=JSON.parse('{"permalink":"/doc/adr/","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"ADR"}')}}]);
---
+++ doc/assets/js/d07df43d.5020cdc5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmithril_doc=self.webpackChunkmithril_doc||[]).push([[2512],{8082:e=>{e.exports=JSON.parse('[{"label":"Draft","permalink":"/doc/adr/tags/draft","count":3},{"label":"Accepted","permalink":"/doc/adr/tags/accepted","count":1}]')}}]);
