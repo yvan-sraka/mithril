0f5ac585ad19ae390cc9b2307765bde6cd29fe69
Alenar <Alenar@users.noreply.github.com>
2023-01-23 10:10:35 +0000

    deploy: 88a16968c3dc701ad33a53a89cca794ac83117dc

---
+++ rust-doc/implementors/lazy_static/trait.LazyStatic.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mithril_common":[["impl <a class=\"trait\" href=\"https://docs.rs/lazy_static/1.4.0/lazy_static/trait.LazyStatic.html\" title=\"trait lazy_static::LazyStatic\">LazyStatic</a> for <a class=\"struct\" href=\"mithril_common/struct.MITHRIL_API_VERSION_REQUIREMENT.html\" title=\"struct mithril_common::MITHRIL_API_VERSION_REQUIREMENT\">MITHRIL_API_VERSION_REQUIREMENT</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
