e11b4cd0ca40bfdb5e949c2006eb90c5d72ca946
jpraynaud <jpraynaud@users.noreply.github.com>
2023-01-26 15:32:49 +0000

    deploy: 909e141c8cbfc0134afc2cd7a8b09898557f8397

--- rust-doc/implementors/core/ops/deref/trait.Deref.js
+++ rust-doc/implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mithril_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mithril_common/struct.MITHRIL_API_VERSION_REQUIREMENT.html\" title=\"struct mithril_common::MITHRIL_API_VERSION_REQUIREMENT\">MITHRIL_API_VERSION_REQUIREMENT</a>"]]
+"mithril_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mithril_common/struct.MITHRIL_API_VERSION_REQUIREMENT.html\" title=\"struct mithril_common::MITHRIL_API_VERSION_REQUIREMENT\">MITHRIL_API_VERSION_REQUIREMENT</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
