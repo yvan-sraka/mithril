b1739a55f6263d88be8d77a0a315a0686cca1dce
jpraynaud <jpraynaud@users.noreply.github.com>
2023-02-10 08:47:40 +0000

    deploy: f7c6d4b2e0753a527b3b50831d1af9732eb3d635

--- rust-doc/implementors/alloc/string/trait.ToString.js
+++ rust-doc/implementors/alloc/string/trait.ToString.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mithril_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"mithril_common/era/enum.SupportedEra.html\" title=\"enum mithril_common::era::SupportedEra\">SupportedEra</a>"]]
+"mithril_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"mithril_common/era/enum.SupportedEra.html\" title=\"enum mithril_common::era::SupportedEra\">SupportedEra</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
