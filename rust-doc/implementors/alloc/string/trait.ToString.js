958e58cbd558c721f805eae6fc2eba18d7df59da
jpraynaud <jpraynaud@users.noreply.github.com>
2023-02-21 09:31:54 +0000

    deploy: d991b2e0a337efb7b9de78c3a00dc4e2dcd0c9ed

--- rust-doc/implementors/alloc/string/trait.ToString.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"mithril_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"mithril_common/era/enum.SupportedEra.html\" title=\"enum mithril_common::era::SupportedEra\">SupportedEra</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ rust-doc/implementors/strum/trait.IntoEnumIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mithril_common":[["impl IntoEnumIterator for <a class=\"enum\" href=\"mithril_common/era/enum.SupportedEra.html\" title=\"enum mithril_common::era::SupportedEra\">SupportedEra</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
