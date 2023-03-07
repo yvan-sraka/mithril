2a00aae493ce75917574e53dacd87dc2ec55afb7
ghubertpalo <ghubertpalo@users.noreply.github.com>
2023-03-07 09:19:05 +0000

    deploy: fa2cf90eb7448246dbb5936614fd3546c0263ff8

--- rust-doc/implementors/mithril_common/sqlite/entity/trait.SqLiteEntity.js
+++ rust-doc/implementors/mithril_common/sqlite/entity/trait.SqLiteEntity.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"mithril_aggregator":[["impl <a class=\"trait\" href=\"mithril_common/sqlite/entity/trait.SqLiteEntity.html\" title=\"trait mithril_common::sqlite::entity::SqLiteEntity\">SqLiteEntity</a> for <a class=\"struct\" href=\"mithril_aggregator/event_store/struct.Event.html\" title=\"struct mithril_aggregator::event_store::Event\">Event</a>"]],
+"mithril_aggregator":[["impl SqLiteEntity for <a class=\"struct\" href=\"mithril_aggregator/event_store/struct.Event.html\" title=\"struct mithril_aggregator::event_store::Event\">Event</a>"]],
 "mithril_common":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
