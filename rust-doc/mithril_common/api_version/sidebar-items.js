e87613f67abf5284143e62a9d28fc98e2b81869f
jpraynaud <jpraynaud@users.noreply.github.com>
2023-03-21 08:52:52 +0000

    deploy: 42fc5073ac71d5560799b502b3db426185577d74

---
+++ rust-doc/mithril_common/api_version/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["APIVersionProviderError","API Version provider error"]],"fn":[["get_open_api_versions_mapping","Build Open API versions mapping"]],"struct":[["APIVersionProvider","API Version provider"]],"type":[["OpenAPIFileName","Open API file name"],["OpenAPIVersionRaw","Open PAI raw version"]]};
