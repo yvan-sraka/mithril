63ae0dad514ecf11fb696722b1471acb45083d04
jpraynaud <jpraynaud@users.noreply.github.com>
2023-04-28 08:12:54 +0000

    deploy: 86ab07d9bb311e02ea62956c0837f439364134de

--- rust-doc/mithril_common/signable_builder/sidebar-items.js
+++ rust-doc/mithril_common/signable_builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["DummyBeacon","DummySignable","DummySignableBuilder"],"trait":["Beacon","Signable","SignableBuilder"]};
+window.SIDEBAR_ITEMS = {"struct":["DummyBeacon","DummySignable","DummySignableBuilder"],"trait":["Artifact","Beacon","Signable","SignableBuilder"]};
