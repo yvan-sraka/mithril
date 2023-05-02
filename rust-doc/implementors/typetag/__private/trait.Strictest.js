38ab677b86503f5c26fc51fa4f23e876b98fbe39
jpraynaud <jpraynaud@users.noreply.github.com>
2023-05-02 16:14:15 +0000

    deploy: cc9660f44b6a36195f7d9990d6a03ab38e6135a6

---
+++ rust-doc/implementors/typetag/__private/trait.Strictest.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mithril_common":[["impl Strictest for dyn <a class=\"trait\" href=\"mithril_common/signable_builder/trait.Artifact.html\" title=\"trait mithril_common::signable_builder::Artifact\">Artifact</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
