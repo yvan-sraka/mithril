824c0065ea7b1fc9cd4ce8abed06172713fc9529
jpraynaud <jpraynaud@users.noreply.github.com>
2023-05-15 08:00:06 +0000

    deploy: 65e508e5a6de1a9bd3c4341d53069d5269cefd83

---
+++ explorer/_next/static/IGbl8UJOHZXmfYAbBbG-g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/882-eda362cae6badf2c.js","static/css/c091b0ecb0a16138.css","static/chunks/pages/index-5325ec57150d1070.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
