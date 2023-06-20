9e7f9481e50febd0edd864c871f2053446b95589
ghubertpalo <ghubertpalo@users.noreply.github.com>
2023-06-20 07:44:00 +0000

    deploy: 699c53424437a5fad1bf99947de274a679156c54

---
+++ explorer/_next/static/BqQW9PiIttaACl-Gh2LYa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/413-11ba6a90284db009.js","static/css/013244c601b8be72.css","static/chunks/pages/index-19a05f62b37ee69f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
