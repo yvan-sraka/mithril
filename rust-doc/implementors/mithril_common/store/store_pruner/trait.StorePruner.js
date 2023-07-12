283cd913725583822419f7e81ed6018c6e661d6e
jpraynaud <jpraynaud@users.noreply.github.com>
2023-07-12 07:32:11 +0000

    deploy: 540bfba7f512420b9def3a53fa0101088d6d178e

--- rust-doc/implementors/mithril_common/store/store_pruner/trait.StorePruner.js
+++ rust-doc/implementors/mithril_common/store/store_pruner/trait.StorePruner.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"mithril_aggregator":[["impl <a class=\"trait\" href=\"mithril_common/store/store_pruner/trait.StorePruner.html\" title=\"trait mithril_common::store::store_pruner::StorePruner\">StorePruner</a> for <a class=\"struct\" href=\"mithril_aggregator/struct.ProtocolParametersStore.html\" title=\"struct mithril_aggregator::ProtocolParametersStore\">ProtocolParametersStore</a>"]],
+"mithril_aggregator":[["impl StorePruner for <a class=\"struct\" href=\"mithril_aggregator/struct.ProtocolParametersStore.html\" title=\"struct mithril_aggregator::ProtocolParametersStore\">ProtocolParametersStore</a>"]],
 "mithril_common":[],
-"mithril_signer":[["impl <a class=\"trait\" href=\"mithril_common/store/store_pruner/trait.StorePruner.html\" title=\"trait mithril_common::store::store_pruner::StorePruner\">StorePruner</a> for <a class=\"struct\" href=\"mithril_signer/struct.ProtocolInitializerStore.html\" title=\"struct mithril_signer::ProtocolInitializerStore\">ProtocolInitializerStore</a>"]]
+"mithril_signer":[["impl StorePruner for <a class=\"struct\" href=\"mithril_signer/struct.ProtocolInitializerStore.html\" title=\"struct mithril_signer::ProtocolInitializerStore\">ProtocolInitializerStore</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
