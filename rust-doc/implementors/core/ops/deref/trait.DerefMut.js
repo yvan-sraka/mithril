e2d08a97dc71cb5bb9bab0a0fad5fac4368b4e3d
Alenar <Alenar@users.noreply.github.com>
2023-08-02 06:52:23 +0000

    deploy: a24b47c7ff6394e9bccbfb4dae59c840462afabc

---
+++ rust-doc/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mithril_common":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"mithril_common/entities/struct.Epoch.html\" title=\"struct mithril_common::entities::Epoch\">Epoch</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
